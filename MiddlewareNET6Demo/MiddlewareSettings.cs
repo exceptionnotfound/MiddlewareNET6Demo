3dffd9a00d96c65e50769b5ec667faddadd1e523
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2022-03-09 13:43:39 -0700

    Added AppSettings toggles for certain Middleware classes.

---
+++ MiddlewareNET6Demo/MiddlewareSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace MiddlewareNET6Demo
+{
+    public class MiddlewareSettings
+    {
+        public bool UseTimeLoggingMiddleware { get; set; }
+        public bool UseCultureMiddleware { get; set; }
+        public bool UseIntentionalDelayMiddleware { get; set; }
+    }
+}
--- MiddlewareNET6Demo/Program.cs
+++ MiddlewareNET6Demo/Program.cs
@@ -1,6 +1,7 @@
 using MiddlewareNET6Demo.Middleware;
 using MiddlewareNET6Demo.Extensions;
 using MiddlewareNET6Demo.Logging;
+using MiddlewareNET6Demo;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,11 +11,12 @@ builder.Services.AddTransient<ILoggingService, LoggingService>();
 
 var app = builder.Build();
 
+var middlewareSettings = builder.Configuration.GetSection("MiddlewareSettings").Get<MiddlewareSettings>();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -27,19 +29,28 @@ app.UseStaticFiles();
 //    await context.Response.WriteAsync("Hello Dear Readers!");
 //});
 
-//Add our custom middleware.
+//At this point, we can add our custom middleware
 //Comment out any you don't want to use.
 //The basic way to add a middleware class to our pipeline is to call UseMiddleware<T>
 app.UseMiddleware<LayoutMiddleware>();
 
 //We can also use custom extensions to add middleware to the pipeline.
 app.UseLoggingMiddleware();
-app.UseTimeLoggingMiddleware();
-app.UseIntentionalDelayMiddleware();
-app.UseCultureMiddleware();
 
-//This one is commented out because it will return a response, and the request will never get to the main app.
+//We can conditionally add middleware to the pipeline based on application settings.
+if(middlewareSettings.UseTimeLoggingMiddleware)
+    app.UseTimeLoggingMiddleware();
+
+if (middlewareSettings.UseCultureMiddleware)
+    app.UseCultureMiddleware();
+
+if(middlewareSettings.UseIntentionalDelayMiddleware)
+    app.UseIntentionalDelayMiddleware();
+
+//The middleware below is commented out because it will return a response,
+//and the request will never get to the main app.
 //Uncomment this if you'd like to see how it behaves.
+
 //app.UseSimpleResponseMiddleware();
 
 app.UseRouting();
